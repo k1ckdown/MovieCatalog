62f2b8dc6f2edee1190e9f8b0a187428f5b38df5
Ivan Semenov <DevSemTsk@gmail.com>
2023-10-21 18:00:55 +0700

    (#19684) Remove localized keys from welcome view moved localized keys from welcome view to LocalizedKeysConstants

---
+++ Movies/Presentation/Common/Constants/LocalizedKeysConstants.swift
@@ -0,0 +1,15 @@
+//
+//  LocalizedKeysConstants.swift
+//  Movies
+//
+//  Created by Ivan Semenov on 21.10.2023.
+//
+
+import Foundation
+
+enum LocalizedKeysConstants {
+    static let welcomeTitle = "WelcomeTitle"
+    static let welcomeBody = "WelcomeBody"
+    static let registration = "Registration"
+    static let logIn = "Log In"
+}
