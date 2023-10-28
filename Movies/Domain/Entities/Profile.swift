69c1dee0e5b80040c514001092d5997a46b03c11
Ivan Semenov <DevSemTsk@gmail.com>
2023-10-29 01:36:33 +0700

    (#18830) Add use case to update user info - updated network service - added profile entity - added UpdateUserInfoUseCase

---
+++ Movies/Domain/Entities/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  Movies
+//
+//  Created by Ivan Semenov on 29.10.2023.
+//
+
+import Foundation
+
+struct Profile {
+    let id: String
+    let nickName: String
+    let email: String
+    let avatarLink: String
+    let name: String
+    let birthDate: String
+    let gender: Gender
+}
