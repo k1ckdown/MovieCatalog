0454314e2e2a5fd08edc72f517dc29f18beb963e
Ivan Semenov <DevSemTsk@gmail.com>
2023-10-29 00:22:06 +0700

    (#18830) Add profile view state - added ProfileViewState - added ProfileViewEvent

---
+++ Movies/Presentation/Scenes/Profile/View/ProfileView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView.swift
+//  Movies
+//
+//  Created by Ivan Semenov on 29.10.2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
