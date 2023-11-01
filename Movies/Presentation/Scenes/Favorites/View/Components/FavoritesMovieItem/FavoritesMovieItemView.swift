0fb0fbeb9afd7a6ff37800e11d36aa81b8d08200
Ivan Semenov <DevSemTsk@gmail.com>
2023-11-01 19:05:53 +0700

    (#18829) Create view and list item view - created FavoritesView - created FavoritesMovieItemView - added a FavoritesMovieItemViewModel with the necessary properties for item view

---
+++ Movies/Presentation/Scenes/Favorites/View/Components/FavoritesMovieItem/FavoritesMovieItemView.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesMovieItemView.swift
+//  Movies
+//
+//  Created by Ivan Semenov on 01.11.2023.
+//
+
+import SwiftUI
+
+struct FavoritesMovieItemView: View {
+
+    enum Size {
+        
+    }
+
+    let viewModel: FavoritesMovieItemViewModel
+
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ Movies/Presentation/Scenes/Favorites/View/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Movies
+//
+//  Created by Ivan Semenov on 01.11.2023.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("Text")
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
