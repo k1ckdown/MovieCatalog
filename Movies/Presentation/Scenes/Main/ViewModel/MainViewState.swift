5ad69d78a31e7473ba90f88d37cc63f076db1402
Ivan Semenov <DevSemTsk@gmail.com>
2023-11-03 04:16:31 +0700

    (#18826) Added loading appearance - added the appearance of loading new elements using ProgressView - instead of tracking the last cell,  the loading indicator is tracked - added new extensions for MainViewState

--- Movies/Presentation/Scenes/Main/ViewModel/MainViewState.swift
+++ Movies/Presentation/Scenes/Main/ViewModel/MainViewState.swift
@@ -5,6 +5,12 @@
 //  Created by Ivan Semenov on 30.10.2023.
 //
 
+enum MainViewEvent {
+    case onAppear
+    case willDisplayLastItem
+    case onSelectMovie(String)
+}
+
 enum MainViewState: Equatable {
     case idle
     case loading
@@ -12,13 +18,44 @@ enum MainViewState: Equatable {
     case loaded(ViewData)
 
     struct ViewData: Equatable {
+        enum LoadMore: Equatable {
+            case failed
+            case available
+            case unavailable
+        }
+
+        var loadMore: LoadMore
+        var listMovies: [MovieDetailsItemViewModel]
         let cardMovies: [MovieDetailsItemViewModel]
-        let listMovies: [MovieDetailsItemViewModel]
     }
 }
 
-enum MainViewEvent {
-    case onAppear
-    case onSelectMovie(String)
-    case willDisplayItem(String)
+extension MainViewState {
+
+    func loadedMore(_ items: [MovieDetailsItemViewModel]) -> Self {
+        guard case .loaded(var viewData) = self else {
+            return self
+        }
+
+        viewData.listMovies.append(contentsOf: items)
+        return .loaded(viewData)
+    }
+
+    func unavailableLoadMore() -> Self {
+        guard case .loaded(var viewData) = self else {
+            return self
+        }
+
+        viewData.loadMore = .unavailable
+        return .loaded(viewData)
+    }
+
+    func failedLoadMore() -> Self {
+        guard case .loaded(var viewData) = self else {
+            return self
+        }
+
+        viewData.loadMore = .failed
+        return .loaded(viewData)
+    }
 }
