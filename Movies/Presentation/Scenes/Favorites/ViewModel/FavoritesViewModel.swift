fde5f7444c3e3bafd8e7f765766d928704a2a5e0
Ivan Semenov <DevSemTsk@gmail.com>
2023-11-02 11:21:12 +0700

    (#18829) Updated FavoritesViewModel - added a method for getting movies

--- Movies/Presentation/Scenes/Favorites/ViewModel/FavoritesViewModel.swift
+++ Movies/Presentation/Scenes/Favorites/ViewModel/FavoritesViewModel.swift
@@ -18,6 +18,28 @@ final class FavoritesViewModel: ViewModel {
     }
 
     func handle(_ event: FavoritesViewEvent) {
-        
+        switch event {
+        case .onAppear:
+            Task { await fetchMovies() }
+        }
+    }
+}
+
+private extension FavoritesViewModel {
+
+    func fetchMovies() async {
+        state = .loading
+
+        do {
+            let movies = try await fetchFavoriteMoviesUseCase.execute()
+            let itemViewModels = movies.map { makeMovieItemViewModel($0) }
+            state = .loaded(.init(movieItems: itemViewModels))
+        } catch {
+            state = .error(error.localizedDescription)
+        }
+    }
+
+    func makeMovieItemViewModel(_ movie: MovieDetails) -> MovieShortItemViewModel {
+        .init(rating: movie.userRating, name: movie.name, imageUrl: movie.poster)
     }
 }
