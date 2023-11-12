10c2af0716a5d80a3a55216daff624c38bc80f8c
Ivan Semenov <DevSemTsk@gmail.com>
2023-11-12 16:33:25 +0700

    Add use case to add review

--- Movies/Domain/UseCases/Review/AddReviewUseCase.swift
+++ Movies/Domain/UseCases/Review/AddReviewUseCase.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+final class AddReviewUseCase {
+
+    private let reviewRepository: ReviewRepositoryProtocol
+    private let keychainRepository: KeychainRepositoryProtocol
+
+    init(
+        reviewRepository: ReviewRepositoryProtocol,
+        keychainRepository: KeychainRepositoryProtocol
+    ) {
+        self.reviewRepository = reviewRepository
+        self.keychainRepository = keychainRepository
+    }
+
+    func execute(review: ReviewModify, movieId: String) async throws {
+        let token = try keychainRepository.retrieveToken()
+        try await reviewRepository.addReview(review, movieId: movieId, token: token)
+    }
+}
