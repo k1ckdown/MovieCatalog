41b6d79d747746e7dc24165216d19aace83713ed
Ivan Semenov <devsemtsk@gmail.com>
2024-02-28 23:44:28 +0700

    Add RealmActor

---
+++ Movies/Data/Realm/RealmActor.swift
@@ -0,0 +1,12 @@
+//
+//  RealmActor.swift
+//  Movies
+//
+//  Created by Ivan Semenov on 28.02.2024.
+//
+
+import Foundation
+
+@globalActor actor RealmActor: GlobalActor {
+    static let shared = RealmActor()
+}
